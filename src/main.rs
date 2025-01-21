9d8df893852c7794b00df1ab6ebaed62107689dc
Jan <jakethedogssandwitch@gmail.com>
2025-01-21 23:00:05 +0100

    Mutable variable

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,13 @@ fn main() {
     println!("Hello, world!");
     
     println!("Added text to check pushing to github");
+    
+    let mut a = 5;
+    
+    println!("{a}");
+
+    a = 6;
+    
+    println!("{a}");
 
 }
