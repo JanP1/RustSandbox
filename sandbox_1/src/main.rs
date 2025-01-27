2b2088d3ef440c697a2062b9f328b7e961b7ed0a
Jan <janp1krc@gmail.com>
2025-01-27 12:17:29 +0100

    Structs

--- sandbox_1/src/main.rs
+++ sandbox_1/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    struct MyStruct {
+        name: String,   
+    }
+
+
+    let mut struct1 = MyStruct{ name:String::from("Jan")};
+
+    let num_taking_ownership = struct1.name;
+    
+    struct1.name = String::from("Jan2");
+    
+    println!("{num_taking_ownership}");
+    println!("{}", struct1.name);
+
+    println!("{num_taking_ownership}");
 }
