5a66bbd3f97f563e484864ca4b0251b90715e28d
Jan <janp1krc@gmail.com>
2025-02-02 12:34:39 +0100

    Working on modules

---
+++ sandbox_2/src/lib.rs
@@ -0,0 +1,8 @@
+#![allow(dead_code, unused_variables)]
+
+mod some_file;
+
+pub fn adding(a1:i32, b1:i32) -> i32 {
+    some_file::add_numbers(a1, b1)
+}
+
--- sandbox_2/src/main.rs
+++ sandbox_2/src/main.rs
@@ -136,23 +136,27 @@
 //}
 //
 //
+//fn main() {
+//    // A counter variable
+//    let mut n = 1;
+//    // Loop while `n` is less than 101
+//    while n < 101 {
+//        if n % 15 == 0 {
+//            println!("fizzbuzz");
+//        } else if n % 3 == 0 {
+//            println!("fizz");
+//        } else if n % 5 == 0 {
+//            println!("buzz");
+//        } else {
+//            println!("{}", n);
+//        }
+//        // Increment counter
+//        n += 1;
+//    }
+//}
+
+use sandbox_2::adding;
+
 fn main() {
-    // A counter variable
-    let mut n = 1;
-    // Loop while `n` is less than 101
-    while n < 101 {
-        if n % 15 == 0 {
-            println!("fizzbuzz");
-        } else if n % 3 == 0 {
-            println!("fizz");
-        } else if n % 5 == 0 {
-            println!("buzz");
-        } else {
-            println!("{}", n);
-        }
-        // Increment counter
-        n += 1;
-    }
+   println!("{}", adding(32, 34)) 
 }
-
-
