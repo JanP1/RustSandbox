7c09f604fe25488b45fc93997f3581b8d458e163
Jan <janp1krc@gmail.com>
2025-02-03 17:27:06 +0100

    Methods

---
+++ RustBookSandbox/methods/src/rectangle.rs
@@ -0,0 +1,20 @@
+#![allow(dead_code)]
+
+
+#[derive(Debug)]
+pub struct Rectangle {
+    pub width: u32,
+    pub height: u32,
+}
+
+impl Rectangle {
+    pub fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+
+    pub fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width &&
+            self.height > other.height
+    }
+}
