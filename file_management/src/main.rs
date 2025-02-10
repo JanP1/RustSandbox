e0dfd4cba45276c63fbea4add0fcdf99864c3f6f
Jan <jakethedogssandwitch@gmail.com>
2025-02-11 00:28:49 +0100

    Reading from a file

--- file_management/src/main.rs
+++ file_management/src/main.rs
@@ -2,7 +2,7 @@ use file_management::json_structures::json_reading::read_todo_from_file;
 
 fn main() {
 
-    match read_todo_from_file("../test.json") {
+    match read_todo_from_file("./test.json") {
         Ok(todo_list) => println!("{:#?}", todo_list),
         Err(e) => eprintln!("Error reading file {}", e),
     }
